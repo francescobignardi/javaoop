c050bbba3a689c28f7998f0623822b4d9e1538f2
Francesco <f.bignardi@hotmail.it>
2023-03-22 17:30:06 +0100

    Aggiungo esercizio

---
+++ exercises/_1/Course.java
@@ -0,0 +1,14 @@
+package javaoop.exercises._1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    String courseName = "Math";
+    Integer maxStudents = 250;
+    Byte qualityRatingOutOf10 = 9;
+
+    List<Student> students = new ArrayList<>();
+
+}
---
+++ exercises/_1/Student.java
@@ -0,0 +1,15 @@
+package javaoop.exercises._1;
+
+public class Student {
+
+    String name = null;
+    Integer age = null;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
