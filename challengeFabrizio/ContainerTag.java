2e34a2c8d8d2d0ec4b3467b2d4efe87befa47970
Francesco <f.bignardi@hotmail.it>
2024-03-04 19:49:40 +0100

    Adding challenge

--- challengeFabrizio/ContainerTag.java
+++ challengeFabrizio/ContainerTag.java
@@ -1,18 +1,31 @@
 package javaoop.challengeFabrizio;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ContainerTag extends Tag{
 
+    protected List<Tag> lista;
+
+    public void addTag(Tag tag){
+        lista.add(tag);
+    }
+
     public ContainerTag(String tag) {
         super(tag);
+        lista = new ArrayList<>();
     }
     @Override
     public void print(){
-        System.out.println("<" + tag + ">");
+        System.out.print("<" + tag);
         for(Map.Entry<String, String> entry : attributes.entrySet()){
-            System.out.println(" <" + entry.getKey() + "> " + entry.getValue() + " </" + entry.getKey() + ">" + "\"");
+            System.out.print(" " + entry.getKey() + "=\"" + entry.getValue() + "\"");
         }
-        System.out.println("</" + tag + ">");
+        System.out.print(">");
+        for(Tag i : lista){
+            i.print();
+        }
+        System.out.print("</" + tag + ">");
     }
 }
