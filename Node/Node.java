4755fccf5df1937289036ed978f52840daf9f92d
Francesco <f.bignardi@hotmail.it>
2024-03-06 20:37:10 +0100

    Added constructor, getters and setters

--- Node/Node.java
+++ Node/Node.java
@@ -4,4 +4,34 @@ public class Node {
     protected Integer value;
     protected Node left;
     protected Node right;
+
+    public Node(Integer value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
